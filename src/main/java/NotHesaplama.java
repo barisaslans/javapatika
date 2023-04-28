a288c32d36b3532756210844989e9386fab24612
Barış Aslan <keskin.deniz088@gmail.com>
2023-04-28 13:43:16 +0300

    first commit

---
+++ src/main/java/NotHesaplama.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class NotHesaplama {
+    public static void main(String[] args) {
+        // Değişkenleri oluştur.
+        int Mat, Fizik, Kimya, Turkce, Muzik, Tarih;
+// Scanner sınıfımızı tanımladık.
+        Scanner input= new Scanner (System.in);
+        //Kullanıcıdan değerleri al.
+        System.out.print("Matematik notunuz : ");
+        Mat = input.nextInt();
+        System.out.print("Fizik notunuz : ");
+        Fizik =input.nextInt();
+        System.out.print("Kimya notunuz : ");
+        Kimya= input.nextInt();
+        System.out.print("Türkçe notunuz : ");
+        Turkce=input.nextInt();
+        System.out.print("Müzik notunuz : ");
+        Muzik=input.nextInt();
+        System.out.print("Tarih notunuz : ");
+        Tarih=input.nextInt();
+        int toplam =(Mat+Fizik+Kimya+Turkce+Muzik+Tarih );
+        double sonuc= toplam/6.0;
+        System.out.println("Ortalamanız :"+ sonuc);
+        int ortalama = (int)sonuc; //Ortalama notu tam sayıya dönüştür.
+        boolean kaldiniz = sonuc < 50;
+        boolean gectiniz = sonuc >= 50;
+        System.out.println("Kaldı mı? " + kaldiniz);
+        System.out.println("Geçti mi? " +  gectiniz);
+    }
+}
