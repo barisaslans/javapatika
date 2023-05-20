3cb0097f0b4ad8b7ad06e05f869ab48a0759b228
Barış Aslan <keskin.deniz088@gmail.com>
2023-05-20 14:10:55 +0300

    sicakligagoreaktivite

---
+++ src/main/java/C11_SicakligaGoreEtkinlik.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class C11_SicakligaGoreEtkinlik {
+
+    public static void main(String[] args) {
+
+        int heat;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Sicaklik giriniz : ");
+        heat = input.nextInt();
+
+        if(heat<=5){
+            System.out.println("Kayak yapabilirsiniz");
+        } else if (heat > 5 && heat <15){
+            System.out.println("Sinemaya gidebilirsiniz");
+        } else if (heat>=15 && heat<25) {
+            System.out.println("Piknige gidebilirsiniz");
+        }else {
+            System.out.println("Yuzmeye gidebilirsiniz");
+        }
+
+
+    }
+}
