c66dc9ec35db6ec2219724b9790e0dd8ed91d407
Barış Aslan <keskin.deniz088@gmail.com>
2023-05-25 10:05:07 +0300

    Burcprogram

---
+++ src/main/java/C13_BurcBulanProgram.java
@@ -0,0 +1,161 @@
+import java.util.Scanner;
+
+public class C13_BurcBulanProgram {
+
+    public static void main(String[] args) {
+
+
+        int day, month;
+        String burc="";
+        boolean isError = false;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Dogdugunuz ayı rakamla giriniz : ");
+        month = input.nextInt();
+
+        System.out.print("Dogdugunuz gunu rakamla giriniz : ");
+        day = input.nextInt();
+
+        if (month==1){
+            if((day>=1 && day <=31)){
+                if(day<22){
+                    burc= "oglak";
+                }else {
+                    burc="kova";
+                }
+            }else {
+                isError=true;
+            }
+        }
+        if (month==2){
+            if (day>=1 && day <=28){
+                if (day<20){
+                    burc="kova";
+                }else {
+                    burc="balık";
+                }
+            }else {
+                isError=true;
+            }
+        }
+        if (month==3){
+            if (day>=1 && day<=31){
+                if (day<21){
+                    burc="Balık";
+                }else {
+                    burc="Koç";
+
+                }
+            }else {
+                isError=true;
+            }
+        }
+        if (month==4){
+            if (day>=1 && day<=30){
+                if (day<21){
+                    burc="Koç";
+                }else {
+                    burc="Boğa";
+                }
+            }else {
+                isError=true;
+            }
+        }
+        if (month==5){
+            if (day>=1 && day<=31) {
+                if (day<22){
+                    burc="Boğa";
+                }else {
+                    burc="İkizler";
+                }
+            }else {
+                isError=true;
+            }
+        }
+
+        if (month==6){
+            if (day>=1 && day<=30) {
+                if (day<22){
+                    burc="İkizler";
+                }else {
+                    burc="Yengeç";
+                }
+            }else {
+                isError=true;
+            }
+        }
+        if (month==7){
+            if (day>=1 && day<=31) {
+                if (day<22){
+                    burc="Yengeç";
+                }else {
+                    burc="Aslan";
+                }
+            }else {
+                isError=true;
+            }
+        }
+        if (month==8){
+            if (day>=1 && day<=31) {
+                if (day<22){
+                    burc="Aslan";
+                }else {
+                    burc="Başak";
+                }
+            }else {
+                isError=true;
+            }
+        }
+        if (month==9){
+            if (day>=1 && day<=30) {
+                if (day<22){
+                    burc="Başak";
+                }else {
+                    burc="Terazi";
+                }
+            }else {
+                isError=true;
+            }
+        }
+        if (month==10){
+            if (day>=1 && day<=31) {
+                if (day<22){
+                    burc="Terazi";
+                }else {
+                    burc="Akrep";
+                }
+            }else {
+                isError=true;
+            }
+        }
+        if (month==11){
+            if (day>=1 && day<=30){
+                if (day<22){
+                    burc="Akrep";
+                }else {
+                    burc="Yay";
+                }
+            }else {
+                isError=true;
+            }
+        }
+        if (month==12){
+            if (day>=1 && day<=31){
+                if (day<22){
+                    burc="Yay";
+                }else {
+                    burc="Oğlak";
+                }
+            }else {
+                isError=true;
+            }
+        }
+        if (isError){
+            System.out.println("Hatali Giris Yaptiniz");
+        }else {
+            System.out.println("Burcunuz : " + burc);
+        }
+
+    }
+}
